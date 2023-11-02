ccf344d64a1ebd67c1ec88f67e01523e6edfe204
Colink02 <colinkinzel@gmail.com>
2023-11-02 03:47:11 -0500

    Made ApiElement into a function instead of class component

--- src/app/api_element.jsx
+++ src/app/api_element.jsx
@@ -1,29 +1,21 @@
+import { useState } from "react";
 
-export class ApiElement extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            api: props.api,
-            x: props.x,
-            y: props.y,
-            width: props.width,
-            height: props.height,
-            title: props.title,
-            description: props.description,
-            id: props.id,
-            selected: false
-        };
-    }
+export function ApiElement() {
+    // The parent of this element (e.g. /api/v1 would have the "api" element as its parent)
+    const [parent, setParent] = useState(null);
+    // The Path name (e.g. /api/v1/test would be "test" with parent of v1)
+    const [apiPath, setApiPath] = useState("");
+    // Type of API (GET, POST, PUT, DELETE, etc.)
+    const [apiMethod, setApiMethod] = useState("");
+    const [description, setDescription] = useState("");
 
-    componentDidMount() {
-        this.dragElement(document.getElementById(this.state.id));
-    }
 
-    dragElement(elmnt) {
-        var pos1 = 0,
-            pos2 = 0,
-            pos3 = 0,
-            pos4 = 0;
-            console.log(elmnt);
-    }
+    return (
+        <div className="api-container">
+            <div className="api-header">
+                <p>{this.props.title}</p>
+            </div>
+            <div className="api-node-description">{props.description}</div>
+        </div>
+    )
 }
