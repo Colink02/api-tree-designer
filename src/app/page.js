9152659a161ebb6c1c8534772f433f46f4ee9a9b
Colink02 <colinkinzel@gmail.com>
2023-11-03 03:24:30 -0500

    Put ReactFlowProvider for the Designer Element in page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+"use client";
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import Designer from './designer'
+import { ReactFlowProvider } from 'reactflow'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Designer />
+      <ReactFlowProvider>
+        <Designer />
+      </ReactFlowProvider>
     </main>
   )
 }
