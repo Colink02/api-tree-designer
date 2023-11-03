58860f236842ecdfaf80af87f3672eadcf67089d
Colink02 <colinkinzel@gmail.com>
2023-11-02 19:52:03 -0500

    starting work on setting up graph like interface similar to node-red

--- src/app/api_element.jsx
+++ src/app/api_element.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 
-export function ApiElement() {
+export function ApiElement({props}) {
     // The parent of this element (e.g. /api/v1 would have the "api" element as its parent)
     const [parent, setParent] = useState(null);
     // The Path name (e.g. /api/v1/test would be "test" with parent of v1)
-    const [apiPath, setApiPath] = useState("");
+    const [apiPath, setApiPath] = useState(props.apiPath);
     // Type of API (GET, POST, PUT, DELETE, etc.)
     const [apiMethod, setApiMethod] = useState("");
-    const [description, setDescription] = useState("");
+    const [description, setDescription] = useState(props.description);
 
+    const onMoveMouse = (e) => {
+
+    };
+    const onMoveDown = (e) => {
+
+    };
+    const onMoveUp = (e) => {
+        
+    };
 
     return (
-        <div className="api-container">
-            <div className="api-header">
-                <p>{this.props.title}</p>
+        <div className="api-node-container draggable">
+            <div className="api-node-header">
+                <p>{apiPath}</p>
             </div>
-            <div className="api-node-description">{props.description}</div>
+            <div className="api-node-description">{description}</div>
         </div>
     )
 }
